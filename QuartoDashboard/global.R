7f9880c2c8850d39def321cde657fbd211cf0fa8
Pipding <pipding@gmail.com>
2024-01-16 19:04:27 +0000

    Add a geographic view of budget data

--- QuartoDashboard/global.R
+++ QuartoDashboard/global.R
@@ -20,9 +20,32 @@ if (!requireNamespace("plotly"))
   install.packages("plotly")
 library(plotly)
 
+if (!requireNamespace("maps")) 
+  install.packages("maps")
+library(maps)
+
+if (!requireNamespace("europop")) 
+  install_github("mdlincoln/europop")
+library(europop)
+
+if (!requireNamespace("fuzzyjoin")) 
+  install.packages("fuzzyjoin")
+library(fuzzyjoin)
+
+if (!requireNamespace("stringdist")) 
+  install.packages("stringdist")
+library(stringdist)
+
 con <- dbConnect(drv=RSQLite::SQLite(), dbname="ICA_2023.sqlite")
 
-customers <- dbGetQuery(con,'SELECT * FROM Customers;')
-developers <- dbGetQuery(con,'SELECT * FROM Developers;')
-projects <- dbGetQuery(con,'SELECT * FROM Projects;')
-assets <- dbGetQuery(con,'SELECT * FROM Assets;')
+customers <- dbGetQuery(con, 'SELECT * FROM Customers;')
+developers <- dbGetQuery(con, 'SELECT * FROM Developers;')
+projects <- dbGetQuery(con, 'SELECT * FROM Projects;')
+assets <- dbGetQuery(con, 'SELECT * FROM Assets;')
+customerCityBudgets <- dbGetQuery(con, '
+SELECT
+  CustomerName,
+  CustomerCity AS City,
+  CustomerCountry AS Country,
+  Budget
+FROM Customers Join Projects P on Customers.CustomerID = P.CustomerID')
